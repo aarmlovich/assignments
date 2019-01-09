a5d6d7f808cd76ef0fb4153c68fb8884d25a4da2
aarmlovich <aarmlovi@gmail.com>
2019-01-08 21:03:26 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+// This part of the code defines a function
+function multiply(a,b) { 
+    return a * b
+}
+
+function add(a,b) { 
+    return a + b
+}
+
+function subtract(a,b) { 
+    return a - b
+}
+
+function divide(a,b) { 
+    return a / b
+}
