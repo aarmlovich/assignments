f378e3bcda7b51887a50445735eb0f578fa17c8e
aarmlovich <aarmlovi@gmail.com>
2019-01-16 11:50:33 -0500

    fix superherocsvmaker for homework

--- superherocsvmaker.py
+++ superherocsvmaker.py
@@ -1,26 +1,34 @@
 import json
-import csv
 
 #read superheroes json
 with open('superheroes.json', 'r') as f:
-    superheroes = json.load(f)
+	superheroes = json.load(f)
 
 
 #save data for each member into a csv row
+import csv
 
+with open('superheroes.csv', 'w') as f:
+	writer = csv.writer(f)
+	#write headers
+	header = ['name', 'age', 'secretIdentity', 'powers', 'squadName' 'homeTown', 'formed', 'secretBase', 'active']
+	writer.writerow(header)
 
-with open('members.csv', 'w') as f:
-    writer = csv.writer(f)
-    #header
-    header = ['name', 'age', 'secretIdentity', 'powers', 'squadName' 'homeTown', 'formed', 'secretBase', 'active']
-    writer.writerow(header)
-    writer.writerow(['col1', 'col2'])
-    writer.writerow(['val1', 'val2'])
-    writer.writerow(['val1', 'val2'])
-    writer.writerow(['val1', 'val2'])
+	# loop over members
+	members = superheroes['members']
+	for member in members:
+		#define variables
+		name = member['name']
+		age = member['age']
+		secretIdentity = member['secretIdentity']
+		powers = member['powers']
+		squadName = superheroes['squadName']
+		homeTown = superheroes['homeTown']
+		formed = superheroes['formed']
+		secretBase = superheroes['secretBase']
+		active = superheroes['active']
+#write data to csv
+		row = [name, age, secretIdentity, powers, squadName, homeTown, formed, secretBase, active]
 
-    # looop thru each member
-members = superheroes['members']
-for member in members:
-	row = [member
-	]
+		writer.writerow(row)
+
